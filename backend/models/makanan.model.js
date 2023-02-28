5b4ca97502f82bc57ba4e2f9d5c90bd4313dbb74
Foliatti <themindthememory78@gmail.com>
2023-02-28 11:57:53 +0700

    added dotenv, model, and db

---
+++ backend/models/makanan.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    origin: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Makanan", schema);
